96d347628ce7a538fa1df149493323e3b9e8726e
Boulama K <boulama@me.com>
2023-07-10 01:33:03 -0400

    Update HasContentEditor.php

--- src/Traits/HasContentEditor.php
+++ src/Traits/HasContentEditor.php
@@ -11,7 +11,6 @@ trait HasContentEditor
         return $defaultEditor::make($field)
             ->label(__('filament-blog::filament-blog.content'))
             ->required()
-            ->toolbarButtons(config('filament-blog.toolbar_buttons'))
             ->columnSpan([
                 'sm' => 2,
             ]);

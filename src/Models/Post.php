b997dd95d95fc6f4f3486cddadb4911542c3df34
Boulama K. <boulama@me.com>
2025-08-06 13:51:03 -0400

    Update Post.php
    add storage facade

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -21,6 +21,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class Post extends Model

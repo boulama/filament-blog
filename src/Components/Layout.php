31036e1cbf0c731ffa394c403489135002f5a5d0
boulama <boulama@me.com>
2025-08-07 13:27:29 -0400

    add og seo

--- src/Components/Layout.php
+++ src/Components/Layout.php
@@ -10,13 +10,19 @@ class Layout extends Component
 
     public function __construct(
         public $post = null,
+        public $og = null,
         public $data = null
     ){}
 
     public function render()
     {
         $setting = Setting::first();
+        if($post !== null) {
+            $this->og['title'] = $post->seo_detail->title ?? $post->title;
+            $this->og['description'] = $post->seo_detail->description ?? $post->sub_title;
+            $this->og['image'] = $post->featurePhoto ?? Storage::disk(config(('FILAMENT_FILESYSTEM_DISK')))->url($post->cover_photo_path);
+        }
 
-        return view('filament-blog::layouts.app', ['setting' => $setting]);
+        return view('filament-blog::layouts.app', ['setting' => $setting, 'og' => $this->og]);
     }
 }
